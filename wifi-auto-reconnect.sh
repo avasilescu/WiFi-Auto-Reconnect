87b9078d4038c192dad3fd121d5b3a9f5470a16f
Alex Vasilescu <alex.a.vasilescu@gmail.com>
2016-01-04 15:42:44 -0800

    Update script to correctly detect a succesful ping. Limit target to google.com for simplicity.

--- wifi-auto-reconnect.sh
+++ wifi-auto-reconnect.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo 'Start Wifi Auto Reconnect Script'
+NOW=$(date +"%m-%d-%y - %T")
+echo $NOW
 
-if ! [ "$(ping -c 4 google.com)" ] && [ "$(ping -c 4 yahoo.com)"]; then
+Target = google.com
 
-	ech 'No Wifi connection. Restarting Wifi ...'
-	
-    sudo /sbin/ifdown wlan0
-    sudo /sbin/ifup wlan0
-
+if ping -c 4 $Target &> /dev/null; then
+	echo 'Wifi connection is up. Restart not required.'
+else
+	echo 'No Wifi connection. Restarting Wifi ...'
+	sudo /sbin/ifdown wlan0
+	sudo /sbin/ifup wlan0
 fi
